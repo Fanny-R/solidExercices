bfeb4397265941ddda7dbc50347164735d702530
Fanny-R <fanny.rottee@gmail.com>
2017-06-22 15:43:47 +0200

    Use decimal instead of double

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -5,15 +5,15 @@ namespace SolidExercices
 {
     public class Calculator
     {
-        public double Calculate(string operation)
+        public decimal Calculate(string operation)
         {
             if(operation.Contains('+'))
             {
                 string[] numbers = operation.Split('+');
-                double result = Convert.ToDouble(numbers[0]);
+                var result = Convert.ToDecimal(numbers[0]);
                 foreach (var number in numbers.Skip(1).ToArray())
                 {
-                    result += Convert.ToDouble(number);
+                    result += Convert.ToDecimal(number);
                 }
                 return result;
 
@@ -21,30 +21,30 @@ namespace SolidExercices
             if (operation.Contains('-'))
             {
                 string[] numbers = operation.Split('-');
-                double result = Convert.ToDouble(numbers[0]);
+                var result = Convert.ToDecimal(numbers[0]);
                 foreach (var number in numbers.Skip(1).ToArray())
                 {
-                    result = result - Convert.ToDouble(number);
+                    result = result - Convert.ToDecimal(number);
                 }
                 return result;
             }
             if (operation.Contains('/'))
             {
                 string[] numbers = operation.Split('/');
-                double result = Convert.ToDouble(numbers[0]);
+                var result = Convert.ToDecimal(numbers[0]);
                 foreach (var number in numbers.Skip(1).ToArray())
                 {
-                    result = result / Convert.ToDouble(number);
+                    result = result / Convert.ToDecimal(number);
                 }
                 return result;
             }
             if (operation.Contains('*'))
             {
                 string[] numbers = operation.Split('*');
-                double result = Convert.ToDouble(numbers[0]);
+                var result = Convert.ToDecimal(numbers[0]);
                 foreach (var number in numbers.Skip(1).ToArray())
                 {
-                    result = result * Convert.ToDouble(number);
+                    result = result * Convert.ToDecimal(number);
                 }
                 return result;
             }
