49717e71bc33c366033778523651a9dab8232443
Fanny-R <fanny.rottee@gmail.com>
2017-06-22 17:23:14 +0200

    Make the calculator truly unaware of the operations

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -12,13 +12,7 @@ namespace SolidExercices
         }
         public decimal Calculate(string calcul)
         {
-            var operations = new List<IOperation>();
-            operations.Add(new Sum());
-            operations.Add(new Substraction());
-            operations.Add(new Division());
-            operations.Add(new Multiplication());
-
-            foreach (var operation in operations)
+            foreach (var operation in _operations)
             {
                 if (operation.CanCalculate(calcul))
                 {
